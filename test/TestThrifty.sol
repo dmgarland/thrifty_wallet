a76adcd2cf46abf2bcf655dcea6aece5d996bed2
Dan Garland <dan@dangarland.co.uk>
2018-10-10 17:50:08 +0100

    A thrifty wallet is just an Etherum account

--- test/TestThrifty.sol
+++ test/TestThrifty.sol
@@ -5,4 +5,15 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Thrifty.sol";
 
 contract TestThrifty {
+   uint public initialBalance = 1 ether;
+
+   Thrifty wallet;
+
+   function beforeAll() public {
+     wallet = new Thrifty(); 
+   }
+
+   function testEmptyWalletHasZeroBalance() public {
+     Assert.equal(address(wallet).balance, 0, "A new, empty wallet should have zro balance");
+   }
 }
